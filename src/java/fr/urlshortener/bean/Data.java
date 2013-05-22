51ef28a551087ac46f32aee073f1142af3feb81a
Moerin <trash.sebastien@gmail.com>
2013-05-22 16:16:42 +0200

    Bean creation to manage database data.

---
+++ src/java/fr/urlshortener/bean/Data.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.urlshortener.bean;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Sebastien
+ */
+public class Data implements Serializable {
+    
+    private long id;
+    private String value;
+
+    public Data() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+    
+    
+    
+}
