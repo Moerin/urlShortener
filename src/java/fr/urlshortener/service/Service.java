9ecf66adc40928e1587b74d430a91d20274f1e01
Moerin <trash.sebastien@gmail.com>
2013-05-23 16:55:57 +0200

    Service MBean Creation

---
+++ src/java/fr/urlshortener/service/Service.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.urlshortener.service;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.annotation.Resource;
+import org.vostok.management.annotations.MBean;
+import org.vostok.management.mbean.MBeanFactory;
+
+/**
+ *
+ * @author Sebastien
+ */
+@MBean
+public class Service {
+    
+    @Resource
+    Service2 bean;
+    
+    @PostConstruct
+    public void loading(){
+        Logger.getLogger(Service.class.getName()).log(Level.INFO, "MBean Service is running... ");
+    }
+    
+}
