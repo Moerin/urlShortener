393d691ec2460d70ee733203441f37eb41b6637d
Moerin <trash.sebastien@gmail.com>
2013-05-24 11:35:44 +0200

    Add field, setter and getter for URI. Delete index field Comment checking path file

--- src/java/fr/urlshortener/configuration/Configuration.java
+++ src/java/fr/urlshortener/configuration/Configuration.java
@@ -4,6 +4,13 @@
  */
 package fr.urlshortener.configuration;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  *
  * @author Sebastien
@@ -11,13 +18,53 @@ package fr.urlshortener.configuration;
 public class Configuration {
     
     private String path;
-
-    public Configuration(String path) { // TODO : Ou passer par un objet Data?
-        this.path = path;
+    private String URI;
+    
+    private boolean fileExist = false;
+    
+    Logger logger = LoggerFactory.getLogger(Configuration.class);
+    
+    /**
+     * Constructeur utilise pour creer une configuration local
+     * @param path
+     * @param index 
+     */
+    public Configuration() {
+//        File f = new File(path);
+//        if(f.exists()){
+//            fileExist = true;
+//        }else{
+//            logger.error("Database file not found");
+//            System.err.println("Database file not found");
+//        }
+    }
+    
+    public String getURI() {
+        return URI;
     }
 
+    /**
+     * Pour chemin d'acces type URI
+     * @return 
+     */
+    public void setURI(String URI) {
+        this.URI = URI;
+    }
+    
+    
     public String getPath() {
         return path;
     }
     
+    /**
+     * Pour chemin d'acces local
+     * @return 
+     */
+    public void setPath(String path) {
+        this.path = path;
+    }
+    
+//    public boolean isFileExist() {
+//        return fileExist;
+//    }
 }
